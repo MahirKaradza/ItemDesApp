8ce9cb79a9bac2ed4cf29ee528d08d56d352e759
MahirKaradza <mahirkaradza@gmail.com>
2017-12-16 01:51:10 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { ItemService } from './item.service';
 
 import { AppComponent } from './app.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import { ItemsComponent } from './items/items.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddItemComponent,
+    ItemsComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
